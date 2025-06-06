6b0c49fe7f3eec0c121b75841ed28aaa7fab4372
AlonsoVera <veraalonso88@gmail.com>
2025-06-06 12:51:08 -0500

    nuevo cambio

--- test_validator.py
+++ test_validator.py
@@ -9,6 +9,6 @@ class TestValidator(unittest.TestCase):
      self.assertFalse(is_valid_address(""))
     def test_not_a_string(self):
      self.assertFalse(is_valid_address(12345))
-     #hola_adios
+     #hola_adios_de_nuevo
     if __name__ == "__main__":
         unittest.main()
